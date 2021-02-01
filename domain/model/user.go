8f3fcc686b3730f332d1a14a8a5c2a4addae2601
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2021-02-01 14:44:14 -0300

    Initial commit

---
+++ domain/model/user.go
@@ -0,0 +1,38 @@
+package model
+
+import (
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type User struct {
+	ID    string `json:"id" valid:"notnull"`
+	Name  string `json:"name" valid:"notnull"`
+	Email string `json:"email" valid:"notnull"`
+}
+
+func (user *User) isValid() error {
+	_, err := govalidator.ValidateStruct(user)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewUser(name string, email string) (*User, error) {
+	user := User{
+		Name:  name,
+		Email: email,
+	}
+
+	user.ID = uuid.NewV4().String()
+
+	err := user.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
